b0113b7af7b81f2db914b63fe166144456d43919
ProkopisKamatsos <prokopiskamatsos@gmail.com>
2026-02-09 01:06:49 +0200

    ASP.NET for beginners

---
+++ ConstosoCrafts/ConstosoCrafts.WebSite/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConstosoCrafts.WebSite.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
